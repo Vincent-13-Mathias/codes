89ba99332348e098891957cfbf02d8372fe08b5a
prometheus@13 <141336856+Vincent-13-Mathias@users.noreply.github.com>
2023-10-31 10:15:34 +0100

    Add files via upload

---
+++ lession-1.js
@@ -0,0 +1,29 @@
+const numbers = [823, 7482, 54156, 516, 546, 98, 7491, 3541, 8423, 1561];
+const a = 823;
+const b = 7482;
+const c = 54156;
+const d = 516;
+const e = 546;
+const f = 98;
+const g = 7491;
+const h = 3541;
+const i = 8423;
+const j = 1561;
+
+const average = (a + b + c + d + e + f + g + h + i + j) / 10;
+console.log(average);
+
+const deviation = (average - a, average - b, average - c, average - d, average - e, average - f,
+                    average - g, average - h, average - i, average - j);
+console.log(deviation + deviation);
+
+const variance = (deviation * deviation);
+console.log(variance);
+
+
+
+
+
+
+
+
